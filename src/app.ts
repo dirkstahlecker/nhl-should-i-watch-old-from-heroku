a0cdb1191fc6eebcf1652e7ba44e0f6fe0497674
Dirk Stahlecker <dirkstahlecker@Dirks-MacBook-Pro.local>
2019-11-03 12:41:57 -0500

    added react client

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import * as express from "express";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('It worked');
