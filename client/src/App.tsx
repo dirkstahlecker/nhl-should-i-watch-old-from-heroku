be04e5fb24c942549d87483e689e40047df18396
Dirk Stahlecker <dirkstahlecker@Dirks-MacBook-Pro.local>
2019-11-03 12:49:02 -0500

    moved client a bit

---
+++ client/src/App.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import './App.css';
+
+import logo from './logo.svg';
+
+class App extends React.Component {
+
+  private getData(): any //TODO
+  {
+    const response = await fetch('/api/hello');
+    console.log(response);
+    return response;
+  }
+
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          Data found from server: 
+          {
+            this.getData()
+          }
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,29 @@ import * as express from "express";
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+// API calls
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+});
+
+app.post('/api/world', (req, res) => {
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
+});
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/src')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/src', 'index.html'));
+  });
+}
+
+
+
 app.get('/', (req, res) => {
   res.send('It worked');
 });
