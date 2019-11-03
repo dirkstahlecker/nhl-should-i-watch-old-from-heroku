0920da8faad473863844ed4120fca7e0fa2b686f
Dirk Stahlecker <dirkstahlecker@Dirks-MacBook-Pro.local>
2019-11-03 12:30:15 -0500

    initial commit to heroku

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import * as express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('It worked');
+});
+
+app.listen(port, () => {
+  // if (err) {
+  //   return console.error(err);
+  // }
+  return console.log(`server is listening on ${port}`);
+});
