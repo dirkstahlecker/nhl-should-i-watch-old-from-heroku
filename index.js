ca0cf4981245cf79487319029d1c9e6fdd870eae
Dirk Stahlecker <dirkstahlecker@Dirks-MacBook-Pro.local>
2019-11-03 13:34:45 -0500

    trying out nhl api

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
 const express = require('express')
-const cowsay = require('cowsay')
 const cors = require('cors')
 const path = require('path')
 
 // Create the server
 const app = express()
 
+const BRUINS_TEAM_ID = 6
+
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, 'client/build')))
 
 // Serve our api route /cow that returns a custom talking text cow
-app.get('/api/cow/:say', cors(), async (req, res, next) => {
-  try {
-    const text = req.params.say
-    const moo = cowsay.say({ text })
-    res.json({ moo })
-  } catch (err) {
-    next(err)
-  }
-})
+// app.get('/api/cow/:say', cors(), async (req, res, next) => {
+//   try {
+//     const text = req.params.say
+//     const moo = cowsay.say({ text })
+//     res.json({ moo })
+//   } catch (err) {
+//     next(err)
+//   }
+// })
 
 // Serve our base route that returns a Hellow World cow
 app.get('/api/cow/', cors(), async (req, res, next) => {
   try {
-    const moo = cowsay.say({ text: 'Hello World!' })
+    const date = "2019-11-02";
+    url = "https://statsapi.web.nhl.com/api/v1/schedule?teamId=" + str(BRUINS_TEAM_ID) + "&date=" + date;
+    console.log(url);
     res.json({ moo })
   } catch (err) {
     next(err)
